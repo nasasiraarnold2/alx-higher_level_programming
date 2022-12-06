9e0f456efea53ecb114e37d311364b2c76daee02
nasasiraarnold2 <nasasiraarnold20@gmail.com>
2022-12-06 08:55:11 -0800

    commit 0ne

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-def print_integers(lst):
-  for i in lst:
-    if isinstance(i, int):
-      print(i)
+def print_list_integer(my_list=[]):
+    for num in my_list:
+        print('{:d}'.format(num))
